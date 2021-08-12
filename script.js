b55705e70acc6974a7f5a5b994814d0848e0bb4b
Satwik Kambham <satwik.kambham@gmail.com>
2021-08-12 10:37:21 +0530

    Intro part done

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("githubLink").addEventListener("click", () => {
+  window.open("https://github.com/code-explorer", "_blank");
+});
+
+document.getElementById("linkedinLink").addEventListener("click", () => {
+    window.open("https://www.linkedin.com/in/satwik-kambham/", "_blank");
+  });
